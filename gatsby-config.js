b0dbaa1c8b11ebce7e846caa84425b33ae0a02a5
Matan Nahmani <matanmatan999new@gmail.com>
2021-01-19 14:33:25 +0200

    fixed google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,24 +32,12 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "G-QVELFD9KSG",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "0",
-        // Defers execution of google analytics script after page load
-        defer: false,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-QVELFD9KSG", // Google Analytics / GA
+        ],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
